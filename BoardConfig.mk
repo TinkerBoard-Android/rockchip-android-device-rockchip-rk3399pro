c792c5b57f076f3d0bfe2fe6d5f751d50084e60d
Weixin Zhou <zwx@rock-chips.com>
2020-05-15 15:54:04 +0800

    BoardConfig: correct sepolicy config and rm npu config
    Change-Id: I470378c38c9848387d16b7d078158dbf0fd08e71
    Signed-off-by: Weixin Zhou <zwx@rock-chips.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -129,4 +129,8 @@ else
 DEVICE_MANIFEST_FILE := device/rockchip/$(TARGET_BOARD_PLATFORM)/manifest.xml
 endif
 
-SCAN_EXCLUDE_DIRS ?= npu
+BOARD_SEPOLICY_DIRS := \
+	device/rockchip/common/sepolicy/vendor \
+	device/rockchip/$(TARGET_BOARD_PLATFORM)/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
+	device/rockchip/common/sepolicy/private
